ce4ab3480dcee337a027eb5247152649434fe4fa
Sydney Ho <sydney.ho215@gmail.com>
2021-07-22 17:46:35 +0000

    .

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getData, getName
 import os
 
 # -- Initialization section --
@@ -14,3 +14,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yoursentiment', methods=['GET','POST'])
+def yoursentiment():
+    userStockChoice = request.form['stockchoice']
+    stock_name = getName(userStockChoice)
+    stock_sentiment = getData(userStockChoice)
+    return render_template('sentiment.html', time = datetime.now(), sentiment = stock_sentiment, name = stock_name)
--- model.py
+++ model.py
@@ -1,2 +1,13 @@
-def getImageUrlFrom(query):
-    pass
+import requests
+
+def getData(query):
+    response = requests.get(f'https://www.styvio.com/api/sentiment/{query}').json()
+    bullish = response['stockTwitsPercentBullish']
+    bearish = response['stockTwitsPercentBearish']
+    neutral = response['stockTwitsPercentNeutral']
+    sentiment = [bullish, bearish, neutral]
+    return sentiment
+
+def getName(query):
+    response = requests.get(f'https://www.styvio.com/api/sentiment/{query}').json()
+    return response['name']
